623e8da25c50d277f6c62fc65106cc91db289a7b
gamrani <gamraniyoussef@gmail.com>
2020-07-13 14:36:23 +0100

    add Yup validation

--- src/components/SignUp/SignupForm.js
+++ src/components/SignUp/SignupForm.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useFormik } from "formik";
-import { validate } from "./SignupFormValidation";
 import InputForm from "./InputForm";
 import "./styles.css";
+import * as Yup from "yup";
 
 export default function SignupForm() {
   const formik = useFormik({
@@ -11,7 +11,15 @@ export default function SignupForm() {
       lastName: "",
       email: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
